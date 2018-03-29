a69b5e1facce52d2f8d6cf9ab6d39ba19cf114d4
Lawrence Hon <lhon001@gmail.com>
2018-03-29 05:51:08 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,18 +10,18 @@ def argue
   argue
 end
 
-def greeting
+def greeting(arg1, arg2)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(arg = "cheese")
+  arg
 end
